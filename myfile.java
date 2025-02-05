916ee69d914837498cf5a57df720e1a9ecf4a6fe
Avishkar <avishkarchandgude22@gmail.com>
2025-02-05 08:20:51 +0000

    commit 2

--- myfile.java
+++ myfile.java
@@ -2,6 +2,6 @@ class myfile
 {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Jenkins...");
+        System.out.println("Welcome to Jenkins server...");
     }
 }
